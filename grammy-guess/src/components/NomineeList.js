d06ee7a580c8abda45ab71d85ed3f745ff6bdc61
itsvitoracacio <vitor.acacio@pm.me>
2022-03-21 20:42:51 -0300

    Fixed routing for award pages that don't have their category name in their own name

--- grammy-guess/src/components/NomineeList.js
+++ grammy-guess/src/components/NomineeList.js
@@ -26,22 +26,9 @@ const NomineeList = ({ categoryName, awardName, userToken, authCreds }) => {
 		fetchClientToken()
 	}, [])
 
-	// Check which of the category names appears in the award name of the current page
-	let currentCategoryName
-	AllAwards.forEach(cat => {
-		void (
-			awardName.includes(cat.categoryName) &&
-			(currentCategoryName = cat.categoryName)
-		)
-	})
-
-	// Find the award object that corresponds to the current award page
-	const currentCategory = AllAwards.find(
-		cat => cat.categoryName === currentCategoryName
-	)
-	const currentAward = currentCategory.categoryAwards.find(
-		award => award.awardName === awardName
-	)
+	let currentAwardObj
+	const currentCategory = AllAwards.find(cat => cat.categoryName === categoryName)
+	const currentAward = currentCategory.categoryAwards.find(award => award.awardName === awardName)
 
 	let isLoggedIn = userToken
 	let token
