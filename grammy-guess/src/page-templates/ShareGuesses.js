b4685b42ae94aadfda953224ae1a34b9918a868d
itsvitoracacio <vitor.acacio@pm.me>
2022-03-27 10:51:03 -0300

    First working versions. Still a lot of bugs to fix, but the main functionality is working

--- grammy-guess/src/page-templates/ShareGuesses.js
+++ grammy-guess/src/page-templates/ShareGuesses.js
@@ -1,4 +1,5 @@
 import UserWinner from '../components/UserWinner'
+import html2canvas from 'html2canvas'
 
 const ShareGuesses = ({ userGuesses, currentPage, setCurrentPage }) => {
 	setCurrentPage('share-screen')
@@ -11,24 +12,39 @@ const ShareGuesses = ({ userGuesses, currentPage, setCurrentPage }) => {
 		userGuessesJsx.push(<UserWinner key={Math.random()} guess={guess} />)
 	})
 
+	const shareSaveToDevice = () => {
+		const downloadBtn = document.querySelector('.shareSaveToDevice')
+		html2canvas(document.querySelector('.shareGuessesPageWrapper')).then(
+			canvas => {
+				downloadBtn.href = canvas.toDataURL('img/png')
+				downloadBtn.download = 'Grammy-GuessTheWinners.png'
+			}
+		)
+	}
+
 	return (
-		<div className='shareGuessesPageWrapper'>
-			{' '}
-			{/* Remove this class and div when we're able to use different css files on different pages -> will also need to change the CSS file for this page */}
-			<div className='shareMyguess'>
-				<p>
-					These are <span className='underline'>my guesses</span> for the{' '}
-					<span className='underline'>GRAMMYs 2022</span>
-				</p>
+		<>
+			<a className='shareSaveToDevice spotifyBtn' onClick={shareSaveToDevice}>
+				Save To Device
+			</a>
+			{/* <button className='shareTweet'>Share on Twitter</button> */}
+			<div className='shareGuessesPageWrapper'>
+				<div className='shareMyGuess'>
+					<p>
+						These are <span className='underline'>my guesses</span> for the{' '}
+						<span className='underline'>GRAMMYs 2022</span>
+					</p>
+				</div>
+				{userGuessesJsx}
+				<div className='footer'>
+					<p>
+						go to <span className='underline'>grammygame.gg</span>, discover the
+						nominees and{' '}
+						<span className='underline'>share your guesses too</span>
+					</p>
+				</div>
 			</div>
-			{userGuessesJsx}
-			<div className='footer'>
-				<p>
-					go to <span className='underline'>grammygame.gg</span>, discover the
-					nominees and <span className='underline'>share your guesses too</span>
-				</p>
-			</div>
-		</div>
+		</>
 	)
 }
 
